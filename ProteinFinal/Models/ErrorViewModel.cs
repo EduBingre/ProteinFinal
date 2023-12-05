313a51dddab8a57b8d2ca6cd6460ad550e2fb3cd
theak <theak@EDUPC>
2023-12-05 15:41:21 +0000

    Add project files.

---
+++ ProteinFinal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProteinFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
