696291b78006026ca2db97e79def2a05a06c45f3
EduBingre <edubingreamaral@gmail.com>
2023-12-07 16:10:08 +0000

    aula 3.

---
+++ Protein.Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Protein.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double? Price { get; set; }
+        public int Calories {  get; set; }  
+        public string? ImageUrl { get; set; }
+    }
+}
